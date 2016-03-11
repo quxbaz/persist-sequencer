d571dc1df0a5b2a190accff86d9aac61dc4c6aef
David Yeung <quxbaz@gmail.com>
2016-03-11 14:35:13 -0500

    Initial commit

---
+++ test/test_channel.js
@@ -0,0 +1,47 @@
+import Channel from 'lib/channel';
+
+describe('Channel', () => {
+
+  let channel;
+
+  beforeEach(() => {
+    channel = new Channel();
+  });
+
+  it("creates default Blip objects that are mute.", () => {
+    channel.state.blips.forEach((blip) => {
+      blip.state.mute.should.be.true;
+    });
+  });
+
+  it("creates default Blip objects with the same @sample state as its own.", () => {
+    let channel = new Channel({sample: 'kick'});
+    channel.state.blips.forEach((blip) => {
+      blip.state.sample.should.eql('kick');
+    });
+  });
+
+  it("does not play a beat if it's mute.", () => {
+    let i = 0;
+    channel.state.blips[0].play = () => i++;
+    channel.playBeat(0);
+    i.should.eql(1);
+    channel.setState({mute: true});
+    channel.playBeat(0);
+    i.should.eql(1);
+    channel.setState({mute: false});
+    channel.playBeat(0);
+    i.should.eql(2);
+  });
+
+  it("creates blips with the correct beat property.", () => {
+    for (let i=0; i < channel.state.beats; i++)
+      channel.state.blips[i].state.beat.should.eql(i);
+  });
+
+  it("Sets a blip with new properties.", () => {
+    channel.setBlip(1, {gain: 4});
+    channel.state.blips[1].state.gain.should.eql(4);
+  });
+
+});
